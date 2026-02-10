bbc5d3f633840de8bc9f863a3be8178f8a62a5a9
breno augusto <breaugustocp@outlook.com>
2026-02-10 14:11:41 -0300

    format creation date in DadosCadastradosTopico; add pagination support and listing endpoint in TopicoController

--- src/main/java/com/alura/forumhub/controller/TopicoController.java
+++ src/main/java/com/alura/forumhub/controller/TopicoController.java
@@ -5,16 +5,20 @@ import com.alura.forumhub.dto.DadosCriacaoTopico;
 import com.alura.forumhub.entity.Topico;
 import com.alura.forumhub.repository.TopicoRepository;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Map;
 
 
 @RestController
 @RequestMapping("/topicos")
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class TopicoController {
 
     private final TopicoRepository repository;
@@ -24,10 +28,9 @@ public class TopicoController {
     }
 
     @PostMapping("/criar-topico")
-    public ResponseEntity<Object> criarTopico(@RequestBody @Valid DadosCriacaoTopico dadosCriacaoTopico,
-                                                              UriComponentsBuilder uriComponentsBuilder) {
-        if (repository.existsByTitulo(dadosCriacaoTopico.titulo())||repository.existsByMensagem(dadosCriacaoTopico.mensagem())) {
-            return ResponseEntity.badRequest().body("Já existe um tópico com esse título e/ou mensagem.");
+    public ResponseEntity<Object> criarTopico(@RequestBody @Valid DadosCriacaoTopico dadosCriacaoTopico, UriComponentsBuilder uriComponentsBuilder) {
+        if (repository.existsByTitulo(dadosCriacaoTopico.titulo()) || repository.existsByMensagem(dadosCriacaoTopico.mensagem())) {
+            return ResponseEntity.badRequest().body(Map.of("mensagem","Já existe um tópico com esse título e/ou mensagem."));
         }
 
         var topico = new Topico(dadosCriacaoTopico);
@@ -36,5 +39,12 @@ public class TopicoController {
         var uri = uriComponentsBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
 
         return ResponseEntity.created(uri).body(new DadosCadastradosTopico(topico));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DadosCadastradosTopico>> listarTopicos(@PageableDefault(sort = {"dataCriacao"}) Pageable pageable) {
+        var topicos = repository.findAll(pageable);
+
+        return ResponseEntity.ok().body(topicos.map(DadosCadastradosTopico::new));
     }
 }
