4ddf180603e329926034f1702965880d19dd0635
breno augusto <breaugustocp@outlook.com>
2026-02-12 20:52:57 -0300

    add JWT authentication; implement TokenService and SecurityFilter for secure login

--- src/main/java/com/alura/forumhub/infrastructure/service/TokenService.java
+++ src/main/java/com/alura/forumhub/infrastructure/service/TokenService.java
@@ -2,7 +2,6 @@ package com.alura.forumhub.infrastructure.service;
 
 import com.alura.forumhub.entity.Usuario;
 import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
@@ -12,7 +11,6 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.Date;
 
 @Service
 public class TokenService {
